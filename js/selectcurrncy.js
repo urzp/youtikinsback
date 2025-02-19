f9f7a31789ba722c1a621957feab2a398f78c316
urzp <ermak80_pass@mail.ru>
2025-02-19 22:56:43 +0300

    save

---
+++ js/selectcurrncy.js
@@ -0,0 +1,52 @@
+
+let currencies = {
+    ru: 'RUB',
+    en: 'EUR',
+    bp: 'BRL',
+    ar: 'EGP',
+    es: 'EUR',
+    fr: 'EUR',
+    de: 'EUR',
+    id: 'USD',
+    hi: 'USD',
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    window.onload = function() { 
+        
+        $('.balance-dropdown__item').click(userSelected)
+        if(localStorage.getItem('userCurrency')!='yes') getCurrency()
+        //getCurrency()
+    }
+});
+
+async function  getCurrency(){
+    let lang = $("html").attr("lang")
+    selectCurrency(lang)
+}
+
+function selectCurrency(lang){
+    let code = currencies[lang]
+    let f_curr = 'no find'
+    $('.balance-dropdown__container #currencies-item').each(function(){
+        if( $(this).text().includes(code) ){ f_curr=$(this).text(); setTimeout( ()=>{$(this).click()} , 500 ) }
+        localStorage.setItem('userCurrency', 'no')
+    })
+    //alertData(lang, code, f_curr, localStorage.getItem('userCurrency') )
+}
+
+function userSelected(){
+    localStorage.setItem('userCurrency', 'yes')
+}
+
+function alertData(lan, curr, find_curr, user){
+    alert(`
+        Lang: ${lan}
+        Currency: ${curr}
+        Find currency: ${find_curr},
+        user set: ${user}
+    `)
+}
+    
+
