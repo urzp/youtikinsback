75906527725f0f20a1f13db312e797713cfd2397
urzp <ermak80_pass@mail.ru>
2025-02-25 16:18:16 +0300

    save

--- server/js/inputcurrencies.js
+++ server/js/inputcurrencies.js
@@ -1,11 +1,24 @@
 let userCurrency 
 let userSymbol
+let dataExchange
 
 document.addEventListener('DOMContentLoaded',  function(){
+    getDataExchange()
     userCurrency = getUserCurrency()
     initInput()
     setTitle()
 })
+
+function getDataExchange(){
+    fetch('https://api.exchangerate-api.com/v4/latest/RUB')
+    .then(response => response.json())
+    .then(data => {
+        dataExchange = data.rates;
+    })
+    .catch(error => {
+      console.error('Ошибка:', error);
+    });    
+}
 
 function getUserCurrency(){
     let symbol = $('.balance-dropdown__name').text().replace(/[0-9]/g, '')
@@ -26,18 +39,21 @@ function setTitle(){
 }
 
 function initInput(){
-    $('#amount').clone().attr({'id':'amount-currency'}).appendTo( "#amount-fields>div" )
+    //$('#amount').clone().attr({'id':'amount-currency'}).prependTo( "#amount-fields>div" )
+    $('#amount').before($('#amount').clone().attr({'id':'amount-currency'}))
     $('#amount').addClass('hiden')
-    $('#amount-currency').attr({type:'number', min:'10'})
+    $('#amount-currency').attr({type:'number', min:'1', step:'0.01'})
     $('#amount-currency').on('input', function() {
         let value = $(this).val()
-        if( value < 0 ) value = value * (-1)
-        $(this).val(value) 
-        value = fx(value).from(userCurrency).to("RUB")
+        if( value < 0 ) { value = value * (-1); $(this).val(value) }
+        //value = fx(value).from(userCurrency).to("RUB")
+        value = Number((value/dataExchange[userCurrency]).toFixed(2))
         $('#amount').val(value)
     })
     $('#amount-currency').on('change', function(){
-        let min_currency =  fx(minAmount).from("RUB").to(userCurrency)
-        if( $(this).val() < min_currency ) { $(this).val(min_currency.toFixed(2)); $('#amount').val(minAmount) }    
+        //let min_currency =  fx(minAmount).from("RUB").to(userCurrency)
+        let min_currency =  Number( (minAmount * dataExchange[userCurrency]).toFixed(2) )
+        //if( $(this).val() < min_currency ) { $(this).val(min_currency); $('#amount').val(minAmount) }   
+        $('#amount-currency').attr( {min: min_currency} )
     })
 }
