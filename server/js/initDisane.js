e7a76c52ffd68f567996719dfc32feecc1754a60
urzp <ermak80_pass@mail.ru>
2025-02-26 19:41:33 +0300

    save

---
+++ server/js/initDisane.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){ 
+    $('<div id="filter-56-title">SELECT</div>').insertAfter( '#dropdown-category-filter-56 .fal.fa-filter' )
+    $('#dropdown-category-filter-56').click() 
+} )
+
